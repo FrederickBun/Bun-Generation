bc74f57d1f3010e6e1b2388bccbbecd032bc730d
磅豆龙 <seventeen@ohdragonboi.cn>
2024-07-06 22:09:53 +0800

    Add master code file

---
+++ master.py
@@ -0,0 +1,11 @@
+import random
+
+name = input("输入软件名称：")
+front = input("请输入激活码前缀，若无请不填：")
+
+for j in range(1, 100):
+    print("您的激活码为~~", front, end="")
+    for i in range(1, 50):
+        user = random.randint(45, 92)
+        print(chr(user), end="")
+    print(" ~~请复制波浪中的内容（不包括波浪和空格）使用此激活码激活/兑换", name)
