c6efcbfa4b6e34d5a1cd7ff4a5a8711877db9980
磅豆龙 <seventeen@ohdragonboi.cn>
2024-07-07 22:11:51 +0800

    Update: The generated code is more accurate and easier to read and recognize

--- master.py
+++ master.py
@@ -6,6 +6,6 @@ front = input("请输入激活码前缀，若无请不填：")
 for j in range(1, 100):
     print("您的激活码为~~", front, end="")
     for i in range(1, 50):
-        user = random.randint(45, 92)
+        user = random.randint(33, 122)
         print(chr(user), end="")
     print(" ~~请复制波浪中的内容（不包括波浪和空格）使用此激活码激活/兑换", name)
